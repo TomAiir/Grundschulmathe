faf0f14d918c4a8ba96ecf5a35db0c245503f09e
Thomas Gwosdek <tomair@Thomass-MBP.fritz.box>
2020-10-22 15:56:19 +0200

    addition of two random numbers with result-alert

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,1 +1,25 @@
 alert('Grundschulmathe')
+
+
+
+for (i = 0; i < 10; i++) {
+const RandomNumber = () => Math.floor(Math.random()*20) + 1;  
+
+
+    const number1 = RandomNumber()
+    const number2 = RandomNumber()
+
+
+    const result = number1 + number2;
+
+    const userInput = prompt('Bitte geben Sie die Lösung aus ' + number1 + ' + ' + number2 + ' ein.' );
+
+    
+
+    if (userInput == result) {
+        alert('Richtig ')
+    }
+    else {
+        alert('Leider falsch ')
+    }
+}
