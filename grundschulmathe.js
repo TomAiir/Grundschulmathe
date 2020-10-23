fbc0ab8472e49e9ac18ec15dfb2af39b6931a3a3
Thomas Gwosdek <tomair@Thomass-MBP.fritz.box>
2020-10-23 17:47:19 +0200

    added Addition-Refactor and task e_final

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -4,11 +4,11 @@ alert('Grundschulmathe: Addition')
 const RandomNumber = (max) => Math.floor(Math.random() * max) + 1;
 
 
-function askAditionTask(mathFn, operatorLiteral) {
+function askAdditionTask(mathFn, operatorLiteral) {
     const summand1 = RandomNumber(20);
     const summand2 = RandomNumber(20);
 
-    const resultAdition = mathFn(summand1, summand2);
+    const resultAddition = mathFn(summand1, summand2);
 
     const userInput = parseInt(
         prompt(`Bitte geben Sie die Lösung aus  ${summand1} ${operatorLiteral} ${summand2} ein.`),
@@ -17,30 +17,30 @@ function askAditionTask(mathFn, operatorLiteral) {
 
 
 
-    if (userInput === resultAdition) {
-        pointsAdition++;
+    if (userInput === resultAddition) {
+        pointsAddition++;
 
-        alert('Richtig ' + pointsAdition + '/5');
+        alert('Richtig ' + pointsAddition + '/5');
     }
     else {
-        alert('Leider falsch ' + pointsAdition + '/5');
+        alert('Leider falsch ' + pointsAddition + '/5');
     }
 }
 
 var i = 0;
-var pointsAdition = 0;
+var pointsAddition = 0;
 
 for (i = 0; i < 5; i++) {
 
-    askAditionTask((a, b) => { return a + b }, '+');
+    askAdditionTask((a, b) => { return a + b }, '+');
 
 }
 
-alert('Sie haben ' + pointsAdition + ' von 5 möglichen Punkten erreicht.')
+alert('Sie haben ' + pointsAddition + ' von 5 möglichen Punkten erreicht.')
 
 var pointsSubtraction = 0;
 
-if (pointsAdition >= 4) {
+if (pointsAddition >= 4) {
     alert('Sie haben Level 2 erreicht: Subtraktion')
     for (i = 0; i < 5; i++) {
         var subtracter1 = RandomNumber(20);
@@ -72,7 +72,93 @@ if (pointsAdition >= 4) {
             alert('Leider falsch ' + pointsSubtraction + '/5')
         }
     }
+    alert('Sie haben ' + pointsSubtraction + ' von 5 möglichen Punkten erreicht.')
 }
 
-alert('Sie haben ' + pointsSubtraction + ' von 5 möglichen Punkten erreicht.')
 
+
+var pointsMultiplication = 0;
+
+if (pointsSubtraction >= 4) {
+    alert('Sie haben Level 3 erreicht: Multiplikation')
+    for (i = 0; i < 5; i++) {
+        var multiplier1 = RandomNumber(10);
+        var multiplier2 = RandomNumber(10);
+        
+        
+        resultMultiplication = multiplier1 * multiplier2;
+            
+        
+
+            const userInput = parseInt(
+                prompt(`Bitte geben Sie die Lösung aus  ${multiplier1} * ${multiplier2} ein.`),
+                10
+            );
+
+
+        if (userInput === resultMultiplication) {
+            pointsMultiplication++;
+
+        }
+
+
+        if (userInput === resultMultiplication) {
+            alert('Richtig ' + pointsMultiplication + '/5')
+        }
+        else {
+            alert('Leider falsch ' + pointsMultiplication + '/5')
+        }
+    }
+    alert('Sie haben ' + pointsMultiplication + ' von 5 möglichen Punkten erreicht.')
+}
+
+
+var pointsDivison = 0;
+
+if (pointsMultiplication >= 4) {
+    alert('Sie haben Level 4 erreicht: Division')
+    for (i = 0; i < 5; i++) {
+        var dividend2 = RandomNumber(10, 1);
+        var dividend1 = (RandomNumber(10, 1)* dividend2);
+        let resultDivision;
+        let question;
+        if (dividend1 >= dividend2) {
+            resultDivision = dividend1 / dividend2
+            question = `Bitte geben Sie die Lösung aus ${dividend1} / ${dividend2} ein.`
+        }
+        else {
+            resultDivision = dividend2 / dividend1
+            question = `Bitte geben Sie die Lösung aus ${dividend2} / ${dividend1} ein.`
+        }
+
+        var userInput = prompt(question);
+
+
+        if (userInput == resultDivision) {
+            pointsDivison++;
+
+        }
+
+
+        if (userInput == resultDivision) {
+            alert('Richtig ' + pointsDivison + '/5')
+        }
+        else {
+            alert('Leider falsch ' + pointsDivison + '/5')
+        }
+    }
+
+    var overallPoints = (pointsAddition + pointsSubtraction + pointsMultiplication + pointsDivison);
+
+    alert('Sie haben ' + pointsDivison + ' von 5 möglichen Punkten erreicht.')
+
+    
+    alert('Sie haben insgesamt ' + (overallPoints) + ' von 20 möglichen Punkten erreicht.')
+
+    if (pointsDivison >= 4) {
+        alert('Glückwunsch! Sie haben bestanden. ')
+    }
+    else {
+        alert('Schade! Sie haben leider nicht bestanden. ')
+    }
+}
